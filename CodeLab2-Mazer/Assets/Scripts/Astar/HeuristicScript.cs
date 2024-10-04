97d0529415a3215581d430e03ce2ace14025fcde
Matthew Parker <maddparker@gmail.com>
2024-10-04 12:23:48 -0400

    Spelled words correctly

---
+++ CodeLab2-Mazer/Assets/Scripts/Astar/HeuristicScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeuristicScript : MonoBehaviour {
+		
+	public virtual float Heuristic(int x, int y, Vector3 start, Vector3 goal, GridScript gridScript){
+		return Random.Range(0, 1000);
+	}
+}
