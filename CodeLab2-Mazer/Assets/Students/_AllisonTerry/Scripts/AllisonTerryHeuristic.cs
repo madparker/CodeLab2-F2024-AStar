f36281a7e5bb34d8bb7aba69083cb1dad281736c
AlleyTerry <art8689@nyu.edu>
2024-10-10 14:03:43 -0400

    i tried lol

--- CodeLab2-Mazer/Assets/Students/_AllisonTerry/Scripts/AllisonTerryHeuristic.cs
+++ CodeLab2-Mazer/Assets/Students/_AllisonTerry/Scripts/AllisonTerryHeuristic.cs
@@ -15,6 +15,7 @@ namespace AllisonTerry
             //this one returns the distance between the two points, start and goal
             //this number then gets added to the cost of the tile to decide the priority in the a star script
             //since each tile will be the same number, the path will be the same every time
+            //unsure on how to get other heuristics
             heursticValue = Mathf.Abs(start.x - goal.x) + Mathf.Abs(start.y - goal.y);
             Debug.Log(heursticValue);
             return heursticValue;
