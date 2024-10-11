6c61d9bc87aeafa94d74064a8e4dd26b254bdd8e
alexandravkanderson <ava8270@nyu.edu>
2024-10-11 07:57:28 -0400

    Fixing Heuristic 1
    scenes, namespace, script created

---
+++ CodeLab2-Mazer/Assets/Students/_AlexandraAnderson/Scripts/AlexandraAndersonFixHeuristic.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlexandraAnderson
+{
+    public class AlexandraAndersonFixHeuristic : HeuristicScript
+    {
+        
+        
+        
+    }
+}
+
